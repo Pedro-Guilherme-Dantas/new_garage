ad699a7f97a5726c3475383bf8dcf266a4944917
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-11-14 01:17:27 -0300

    Iniciando arquitetura para fornt-end e iniciando implementação da homepage

---
+++ src/main/java/br/ufrn/imd/new_garage/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.new_garage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String homePage() {
+		return "home";
+	}
+}
