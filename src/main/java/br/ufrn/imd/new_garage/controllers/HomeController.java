612e4b6220392dc945e71802c2ca0a37e9ffdb4a
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-11-14 11:27:45 -0300

    Pequenas atualizações na HomeController

--- src/main/java/br/ufrn/imd/new_garage/controllers/HomeController.java
+++ src/main/java/br/ufrn/imd/new_garage/controllers/HomeController.java
@@ -2,11 +2,14 @@ package br.ufrn.imd.new_garage.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 	
-	@GetMapping("/")
+	@GetMapping
 	public String homePage() {
 		return "home";
 	}
