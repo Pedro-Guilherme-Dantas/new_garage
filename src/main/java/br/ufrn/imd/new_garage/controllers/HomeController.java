c8bea92e94f50eac7955a533f78063d2dd27f69c
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-11-15 01:56:46 -0300

    começando a implemtentação da tela inicial como uma página dinâmica

--- src/main/java/br/ufrn/imd/new_garage/controllers/HomeController.java
+++ src/main/java/br/ufrn/imd/new_garage/controllers/HomeController.java
@@ -1,16 +1,26 @@
 package br.ufrn.imd.new_garage.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import br.ufrn.imd.new_garage.entities.Servico;
+import br.ufrn.imd.new_garage.repositories.ServicoRepository;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	@Autowired
+    public ServicoRepository repository;
 	
 	@GetMapping
-	public String homePage() {
+	public String homePage(Model model) {
+		List<Servico> servicos = repository.findAll();
+		model.addAttribute("servicos", servicos);
 		return "home";
 	}
 }
