bd6a98d2d71ed374bbba7569f3a03d1311b639ce
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-11-17 01:30:59 -0300

    adicionando método para adicionar novo serviço

--- src/main/java/br/ufrn/imd/new_garage/controllers/ServicoController.java
+++ src/main/java/br/ufrn/imd/new_garage/controllers/ServicoController.java
@@ -2,15 +2,16 @@ package br.ufrn.imd.new_garage.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.ufrn.imd.new_garage.dto.ServicoDTO;
 import br.ufrn.imd.new_garage.entities.Moto;
@@ -37,14 +38,25 @@ public class ServicoController {
     
     @GetMapping("/novo")
     public String novoServico(ServicoDTO servicoDTO, Model model) {
-    	List<Moto> motos = motoRepository.findAll();
-    	model.addAttribute("motos", motos);
+    	setMotos(model);
     	
     	return "servico/novo";
     }
     
-    public String create() {
-    	return "todo";
+    @PostMapping("/novo")
+    public String insert(@Valid ServicoDTO servicoDTO, BindingResult result, Model model) {
+    	// Validando preenchimento do formulário
+        if(result.hasErrors()) {
+        	setMotos(model);
+        	
+			return "servico/novo";
+		}
+		// salvando novo servico
+		Servico servico = servicoDTO.toServico();
+		System.out.println(servico);
+		repository.save(servico);
+
+		return "redirect:/servico/novo";
     }
     
     @GetMapping(value = "/{id}")
@@ -52,9 +64,9 @@ public class ServicoController {
         Servico result = repository.findById(id).get();
         return result;
     }
-    @PostMapping("/insert")
-    public Servico insert(@RequestBody Servico servico) {
-        Servico result = repository.save(servico);
-        return result;
+    
+    private void setMotos(Model model) {
+    	List<Moto> motos = motoRepository.findAll();
+    	model.addAttribute("motos", motos);
     }
 }
