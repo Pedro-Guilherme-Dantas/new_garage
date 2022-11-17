b5a13170f467f69877f2302ca08baaa65146ff3d
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-11-17 01:28:05 -0300

    refatoração do DTO

--- src/main/java/br/ufrn/imd/new_garage/dto/ServicoDTO.java
+++ src/main/java/br/ufrn/imd/new_garage/dto/ServicoDTO.java
@@ -6,58 +6,28 @@ import javax.validation.constraints.Size;
 import javax.validation.constraints.Min;
 
 import java.util.Calendar;
-import java.util.Optional;
-
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import br.ufrn.imd.new_garage.entities.Cliente;
 import br.ufrn.imd.new_garage.entities.EnumStatusServico;
 import br.ufrn.imd.new_garage.entities.EnumTipoServico;
 import br.ufrn.imd.new_garage.entities.Moto;
 import br.ufrn.imd.new_garage.entities.Servico;
-import br.ufrn.imd.new_garage.repositories.ClienteRepository;
-import br.ufrn.imd.new_garage.repositories.MotoRepository;
 
 public class ServicoDTO {
-	private long id;
-	
-	@NotBlank
 	@Min(value=0)
 	private double valor;
     
 	@NotBlank
-	@Size(min=6)
+	@Size(min=6, max=255)
 	private String descricao;
-    private Calendar data_inicio;
-    private Calendar data_fim;
-    private EnumStatusServico statusServico;
     
     @NotNull
     private EnumTipoServico tipoServico;
     
-    @NotBlank
-    private long clienteId;
-    
-    @NotBlank
+    @NotNull
     private long motoId;
+    private Moto moto = new Moto();
     
-    @Autowired
-    public MotoRepository motoRepository;
-    
-    @Autowired
-    public ClienteRepository clienteRepository;
-    
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
 	public double getValor() {
 		return valor;
 	}
@@ -70,35 +40,11 @@ public class ServicoDTO {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	public Calendar getData_inicio() {
-		return data_inicio;
-	}
-	public void setData_inicio(Calendar data_inicio) {
-		this.data_inicio = data_inicio;
-	}
-	public Calendar getData_fim() {
-		return data_fim;
-	}
-	public void setData_fim(Calendar data_fim) {
-		this.data_fim = data_fim;
-	}
-	public EnumStatusServico getStatusServico() {
-		return statusServico;
-	}
-	public void setStatusServico(EnumStatusServico statusServico) {
-		this.statusServico = statusServico;
-	}
 	public EnumTipoServico getTipoServico() {
 		return tipoServico;
 	}
 	public void setTipoServico(EnumTipoServico tipoServico) {
 		this.tipoServico = tipoServico;
-	}
-	public long getClienteId() {
-		return clienteId;
-	}
-	public void setClienteId(long clienteId) {
-		this.clienteId = clienteId;
 	}
 	public long getMotoId() {
 		return motoId;
@@ -106,20 +52,26 @@ public class ServicoDTO {
 	public void setMotoId(long motoId) {
 		this.motoId = motoId;
 	}
+	public Moto getMoto() {
+		return moto;
+	}
+	public void setMoto(Moto moto) {
+		this.moto = moto;
+	}
+	public void setMotoById(long id) {
+		this.moto.setId(id);
+	}
 	
 	public Servico toServico() {
-		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
-		Optional<Moto> moto = motoRepository.findById(motoId);
-
+		moto.setId(motoId);
+		
 		Servico servico = new Servico();
-		servico.setId(id);
 		servico.setValor(valor);
-		servico.setData_inicio(data_inicio);
+		servico.setData_inicio(Calendar.getInstance());
 		servico.setDescricao(descricao);
 		servico.setTipoServico(tipoServico);
 		servico.setStatusServico(EnumStatusServico.PENDENTE);
-		servico.setCliente(cliente.orElse(null));
-		servico.setMoto(moto.orElse(null));
+		servico.setMoto(moto);
 		
 		return servico;
 	}
