1c2d0fbee8115e7aba43cbc4a6b1a38b0de4059c
Gustavo Gorgônio <ggorgon03@gmail.com>
2022-12-05 22:08:53 -0300

    Adicionando listagem de motos

--- src/main/java/br/ufrn/imd/new_garage/controllers/MotoController.java
+++ src/main/java/br/ufrn/imd/new_garage/controllers/MotoController.java
@@ -2,6 +2,8 @@ package br.ufrn.imd.new_garage.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,16 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ufrn.imd.new_garage.entities.Moto;
 import br.ufrn.imd.new_garage.repositories.MotoRepository;
 
-@RestController
+@Controller
 @RequestMapping(value = "/moto")
 public class MotoController {
     @Autowired
     public MotoRepository repository;
 
-    @GetMapping("/list")
-    public List<Moto> findAll() {
-        List<Moto> result = repository.findAll();
-        return result;
+    @GetMapping()
+    public String index(Model model) {
+        List<Moto> motos = repository.findAll();
+        model.addAttribute("motos", motos);
+        
+        return "moto/motos";
     }
     @GetMapping(value = "/{id}")
     public Moto findById(@PathVariable Long id) {
@@ -32,5 +36,5 @@ public class MotoController {
     public Moto insert(@RequestBody Moto moto) {
         Moto result = repository.save(moto);
         return result;
-    }   
+    }
 }
